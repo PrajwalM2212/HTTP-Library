fd327d7b1e6182e5f9fc97f3a7769deed9d0d0ce
PrajwalM2212 <prajwalmmath@gmail.com>
2018-07-21 14:28:42 +0530

    AJAX lib

---
+++ app.js
@@ -0,0 +1,52 @@
+const eHttp = new easyHttp();
+
+/*
+eHttp.get("https://jsonplaceholder.typicode.com/posts",
+    function (err, response) {
+        if (!err) {
+            console.log(response);
+        } else {
+            console.log(err);
+        }
+    });
+
+*/
+
+/*
+eHttp.post("https://jsonplaceholder.typicode.com/posts",
+    {
+        title: "in quibusdam tempore odit est dolorem",
+        body: "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+
+    }, function (err, response) {
+        if (!err) {
+            console.log(response);
+        } else {
+            console.log(err);
+        }
+    });
+
+*/
+
+/*
+eHttp.put("https://jsonplaceholder.typicode.com/posts/5", {
+    title: "in quibusdam tempore odit est dolorem",
+    body: "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+}, function(err,response){
+
+    (err) ? console.log(err):console.log(response);
+
+});*/
+
+/*
+eHttp.delete("https://jsonplaceholder.typicode.com/posts/3",
+function(err,response){
+    if (!err) {
+        console.log(response);
+    } else {
+        console.log(err);
+    } 
+});
+console.log(eHttp);*/
+
+
